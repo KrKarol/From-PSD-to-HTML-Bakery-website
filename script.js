77016cd60f058b8cb63242dc1358e7b64f41fdb7
admin <admin@MacBook-Pro-admin.local>
2021-10-22 20:38:02 +0200

    Finished HTML + CSS

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let hamburger = document.querySelector(".hamburger");
-let nav = document.querySelector("nav");
+let nav = document.querySelector(".navbarMain");
 
 
 hamburger.addEventListener("click", function (){
 
     hamburger.classList.toggle("isactive");
-    nav.classList.toggle("active")
+    nav.classList.toggle("navActivation")
+
+    if(nav.classList.contains("navActivation")){
+        document.querySelector("body").style = "position:fixed"
+    } else document.querySelector("body").style = "";
 
 })
